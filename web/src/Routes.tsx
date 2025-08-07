f1d71c993ac52f608e378c54f6ab283f3ce45c96
Renan <renan.santosoliveira0@gmail.com>
2025-08-07 14:33:38 -0300

    feat: configure routes for Login, Home and Recipe pages

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -8,11 +8,17 @@
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
 import { Router, Route } from '@redwoodjs/router'
-
+import HomePage from './pages/HomePage/HomePage'
+import LoginPage from './pages/LoginPage/LoginPage'
+import RecipePage from './pages/RecipePage/RecipePage'
 const Routes = () => {
   return (
     <Router>
       <Route notfound page={NotFoundPage} />
+      <Route path="/" page={HomePage} name="home" />
+      <Route path="/login" page={LoginPage} name="login" />
+      <Route path="/recipe/{id:Int}" page={RecipePage} name="recipe" />
+
     </Router>
   )
 }
