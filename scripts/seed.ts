a288a3245d21fd077b123fff33472246f15f8d48
Renan <renan.santosoliveira0@gmail.com>
2025-08-07 14:25:43 -0300

    chore: add seed data for categories, tags

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,27 +1,43 @@
-// import { db } from 'api/src/lib/db'
-
-// Manually apply seeds via the `yarn rw prisma db seed` command.
-//
-// Seeds automatically run the first time you run the `yarn rw prisma migrate dev`
-// command and every time you run the `yarn rw prisma migrate reset` command.
-//
-// See https://redwoodjs.com/docs/database-seeds for more info
+import { db } from 'api/src/lib/db'
 
 export default async () => {
   try {
-    // Create your database records here! For example, seed some users:
-    //
-    // const users = [
-    //   { name: 'Alice', email: 'alice@redwoodjs.com' },
-    //   { name: 'Bob', email: 'bob@redwoodjs.com' },
-    // ]
-    //
-    // await db.user.createMany({ data: users })
+    const categories = [
+      { name: 'Café da Manhã' },
+      { name: 'Almoço' },
+      { name: 'Jantar' },
+      { name: 'Sobremesa' },
+      { name: 'Lanche' },
+    ]
 
-    console.info(
-      '\n  No seed data, skipping. See scripts/seed.ts to start seeding your database!\n'
-    )
+    const tags = [
+      { name: 'Vegano' },
+      { name: 'Sem Glúten' },
+      { name: 'Rápido' },
+      { name: 'Saudável' },
+      { name: 'Conforto' },
+    ]
+
+    for (const category of categories) {
+      await db.category.upsert({
+        where: { name: category.name },
+        update: {},
+        create: category,
+      })
+    }
+
+    for (const tag of tags) {
+      await db.tag.upsert({
+        where: { name: tag.name },
+        update: {},
+        create: tag,
+      })
+    }
+
+    console.info('Seed data inserted: Categories and Tags.')
   } catch (error) {
-    console.error(error)
+    console.error('Error inserting seed data:', error)
+  } finally {
+    await db.$disconnect()
   }
 }
